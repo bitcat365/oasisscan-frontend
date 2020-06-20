0dd7878cb86e5c15d514ab67706b10e0c80bf398
liuzhi <liuzhi@huobis-MacBook-Pro.local>
2020-06-20 21:55:32 +0800

    update

--- pages/validators/detail/_id.vue
+++ pages/validators/detail/_id.vue
@@ -105,7 +105,7 @@
               <div class="total-con">
                 Total
                 <div class="total-value">
-                  <div class="value"> {{escrowAmountStatus.total}} ROSE</div>
+                  <div class="value"> {{escrowAmountStatus.total}} AMBER</div>
                   <div class="share"> ({{escrowSharesStatus.total}} shares)</div>
                 </div>
               </div>
