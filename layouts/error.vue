6108717fb2315854213835adb6718bd9b1cb11fd
silmazing <lzg2903@gmail.com>
2020-07-23 00:01:25 +0800

    增加404页面

---
+++ layouts/error.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+    <nav-bar :active="-1"/>
+    <div class="notfound-con">
+      <img class="notfound" src="../assets/404_page.png">
+    </div>
+  </div>
+</template>
+
+<script>
+  import NavBar from '../components/NavigationBar'
+
+  export default {
+    name: 'error',
+    components: { NavBar }
+  }
+</script>
+
+<style lang="scss" scoped>
+  @import "../assets/css/common";
+  .notfound {
+    width: rem(220);
+    height: rem(220);
+  }
+  .container {
+    min-height: calc(100vh - #{rem(100)});
+    flex-direction: column;
+    display: flex;
+  }
+  .notfound-con {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex: 1;
+  }
+</style>
