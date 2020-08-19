6e0ab848080f894d0fea194a6b1fe98eb1a6177d
liuzhi <liuzhi@huobis-MacBook-Pro.local>
2020-08-19 10:42:07 +0800

    title change

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'Oasis Block Explorer',
+    title: 'OASIS SCAN | Oasis Block Explorer',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
