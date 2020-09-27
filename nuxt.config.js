f9c5d44d2f4935bb0b5a33c6f209439b01e2978b
liuzhi <liuzhi@huobi.com>
2020-09-27 23:46:34 +0800

    修改title

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'OASIS SCAN | Oasis Block Explorer',
+    title: 'OASIS SCAN | Oasis Blockchain Explorer',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
