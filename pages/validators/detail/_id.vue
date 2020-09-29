4a1d690b7acd875e6c23fc5075f629311b367d23
liuzhi <liuzhi@huobi.com>
2020-09-29 18:29:25 +0800

    add link

--- pages/validators/detail/_id.vue
+++ pages/validators/detail/_id.vue
@@ -114,8 +114,8 @@
           <div v-if="!noExtraInfo" class="desc extra-info-con">
             <div class="label">Socal Media</div>
             <a v-if="website" target="_blank" :href="website" title="website"><img class="extra-icon" src="../../../assets/validator_website.svg" /></a>
-            <a v-if="twitter" target="_blank" :href="twitter" title="twitter"><img class="extra-icon" src="../../../assets/validator_twitter.svg" /></a>
-            <a v-if="keybase" target="_blank" :href="keybase" title="keybase"><img class="extra-icon" src="../../../assets/validator_keybase.svg" /></a>
+            <a v-if="twitter" target="_blank" :href="`https://twitter.com/${twitter}`" title="twitter"><img class="extra-icon" src="../../../assets/validator_twitter.svg" /></a>
+            <a v-if="keybase" target="_blank" :href="`https://keybase.io/${keybase}`" title="keybase"><img class="extra-icon" src="../../../assets/validator_keybase.svg" /></a>
           </div>
           <a :href="editURL" target="_blank" class="edit">
             UPDATE VALIDATOR INFO?
