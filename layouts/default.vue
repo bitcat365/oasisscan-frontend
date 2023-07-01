9c3f24e4d9da8ce4ca98b1b15abc85498108c844
‘baichenyang’ <baichenyang_ah@bonc.com.cn>
2023-07-01 09:51:58 +0800

    style

--- layouts/default.vue
+++ layouts/default.vue
@@ -29,8 +29,8 @@ export default {
 }
 </script>
 <style lang="scss">
-@import '../assets/css/common';
 #page-container {
+  @include fontFamily;
   min-height: 100vh;
   background-color: $page-background;
   display: flex;
@@ -42,79 +42,5 @@ export default {
   .page-header {
     margin: rem(20) 0;
   }
-}
-body,
-button,
-input,
-select,
-textarea,
-a {
-  color: $n-color;
-}
-a:visited,
-a:active {
-  color: $n-color;
-}
-html {
-  font-size: $rem-base + px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
-@media (-webkit-min-device-pixel-ratio: 2), (min-resolution: 192dpi) {
-  /* Retina-specific stuff here */
-  html {
-    font-size: $rem-base/1 + px;
-  }
-}
-body {
-  @include fontFamily;
-}
-#__layout {
-  position: relative;
-  min-height: 100vh;
-  // padding-bottom: rem(100);
-  // min-width: rem(1280);
-}
-*,
-*:before,
-*:after {
-  box-sizing: border-box;
-  margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
-.ivu-spin-dot {
-  background-color: #ff3f0f;
 }
 </style>
