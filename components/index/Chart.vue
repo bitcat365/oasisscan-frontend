aed68d5e61fd20e3861d0ac187531576464beee2
‘baichenyang’ <baichenyang_ah@bonc.com.cn>
2023-07-13 00:06:54 +0800

    style

--- components/index/Chart.vue
+++ components/index/Chart.vue
@@ -11,7 +11,7 @@ import { getMonth, readable } from '../../utils'
 export default {
   name: 'Chart',
   props: {
-    txHistory: {
+    tx: {
       type: Array,
       default: function() {
         return []
@@ -25,9 +25,9 @@ export default {
     }
   },
   data() {
-    const days = this.txHistory.length
+    const days = this.tx.length
     const daysArray = []
-    const latest = days > 0 ? this.txHistory[days - 1].key * 1000 : +new Date()
+    const latest = days > 0 ? this.tx[days - 1].key * 1000 : +new Date()
     for (let day = 0; day < days; day++) {
       const thatDay = new Date(latest)
       thatDay.setDate(new Date(latest).getDate() - day)
@@ -36,7 +36,7 @@ export default {
     daysArray.reverse()
 
     const lastDayFinished = new Date(latest).setHours(23, 59, 59, 999) < Date.now()
-    let data = this.txHistory.map(h => h.value)
+    let data = this.tx.map(h => h.value)
     let finishedDaysLength = days
     if (!lastDayFinished && days > 0) {
       const unfinishedDayFraction = (latest - new Date(latest).setHours(0, 0, 0, 0)) / (24 * 60 * 60 * 1000)
@@ -58,66 +58,95 @@ export default {
       })
     }
 
-      // console.log('daysArray', daysArray)
-      return {
-        chartOptions: {
-          chart: {
-            type: 'areaspline',
-          },
+    // console.log('daysArray', daysArray)
+    return {
+      chartOptions: {
+        chart: {
+          type: 'areaspline'
+        },
+        title: {
+          text: ''
+        },
+        legend: {
+          enabled: false
+        },
+        xAxis: {
           title: {
             text: ''
           },
-          legend: {
-            enabled: false
-          },
-          xAxis: {
-            title: {
-              text: ''
-            },
-            categories: [
-              ...daysArray
-            ],
-            labels: {
-              step: 3
-            }
-          },
-          yAxis: {
-            title: {
-              text: ''
-            }
-          },
-          tooltip: {
-            shared: false,
-            valueSuffix: '',
-            formatter: function () {
-              return this.point.tooltip || readable(this.y)
-            }
-          },
-          credits: {
-            enabled: false
-          },
-          plotOptions: {
-            areaspline: {
-              fillOpacity: 0.2,
-              color: '#7A5AF8',
-              marker: {
-                enabled: false
-              }
-            }
-            // TODO #53B1FD
-          },
-          series: [ {
+          categories: [...daysArray],
+          labels: {
+            step: 3
+          }
+        },
+        yAxis: {
+          title: {
+            text: ''
+          }
+        },
+        tooltip: {
+          backgroundColor: 'rgba(52, 64, 84, 0.7)',
+          borderWidth: 0,
+          shared: false,
+          borderRadius: 10,
+          formatter: function() {
+            return this.point.tooltip || readable(this.y)
+            // return `<span style="font-size:12px;font-weight:600;color:#fff">${this.x}</span>
+            // <br/>
+            // <span style="font-size:12px;color:#E4E7EC">$${readable(this.y)}</span>`
+          }
+        },
+        credits: {
+          enabled: false
+        },
+        colors: ['#7A5AF8', '#53B1FD'],
+        // plotOptions: {
+        //   areaspline: {
+        //     color: '#7A5AF8',
+        //     marker: {
+        //       enabled: false
+        //     }
+        //     // TODO #53B1FD
+        //   }
+        // },
+        series: [
+          {
             name: '',
             data: data,
-            zoneAxis: 'x',
-            zones: [{ value: finishedDaysLength - 1 }, { dashStyle: 'shortdot', fillColor: 'transparent' }]
+            marker: {
+              enabled: false
+            },
+            // zoneAxis: 'x',
+            // zones: [{ value: finishedDaysLength - 1 }, { dashStyle: 'shortdot', fillColor: 'transparent' }],
+            fillColor: {
+              linearGradient: [0, 0, 0, 300],
+              stops: [
+                // [0, Highcharts.getOptions().colors[0]],
+                [0, 'rgba(122,90,248,0.3)'],
+                [0.6, 'rgba(122,90,248,0)']
+              ]
+            }
+          },
+          {
+            name: '',
+            data: [10000, 80000, 100000, 60000, 200000, 160000, 180000, 120000],
+            marker: {
+              enabled: false
+            },
+            fillColor: {
+              linearGradient: [0, 0, 0, 300],
+              stops: [
+                [0, 'rgba(83,177,253,0.3)'],
+                [0.6, 'rgba(83,177,253,0)']
+              ]
+            }
           }
         ]
       }
     }
   },
   mounted() {
-    // console.log('this.$refs.chart',this.$refs, this.txHistory)
+    // console.log('this.$refs.chart',this.$refs, this.tx)
   }
 }
 </script>
