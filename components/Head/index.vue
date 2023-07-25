d40a53d3ca3b65869e29620e62fc9aa0ca6970f6
‘baichenyang’ <baichenyang_ah@bonc.com.cn>
2023-07-26 00:33:42 +0800

    Head

---
+++ components/Head/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="Head-wrapper">
+    <div class="Head-left">
+      <span>{{ title }}</span>
+      <slot name="HeadLeft"></slot>
+    </div>
+    <div class="Head-right">
+      <slot name="HeadRight"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Head',
+  props: {
+    title: {
+      type: String,
+      default: ''
+    }
+  },
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../assets/css/utils';
+.Head-wrapper {
+  @extend .flexRow;
+  height: rem(40);
+  line-height: rem(40);
+  margin-bottom: rem(20);
+  .Head-left {
+    @include fontFamily-Poppins;
+    font-size: rem(24);
+  }
+  .Head-right {
+    font-size: rem(16);
+  }
+}
+</style>
