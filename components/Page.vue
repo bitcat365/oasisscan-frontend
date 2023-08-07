26d00740a2165162c337f5b19262a298ab09dafa
‘baichenyang’ <baichenyang_ah@bonc.com.cn>
2023-08-08 00:26:41 +0800

    page methods

--- components/Page.vue
+++ components/Page.vue
@@ -6,19 +6,27 @@
     <div class="sim-btn-icon" @click="previous">
       <SvgIcon className="svgIcon" iconName="left" />
     </div>
-    <template v-if="total <= 5">
-      <div :class="i=== page ? 'sim-btn-num sim-btn-active':'sim-btn-num'" v-for="i in total">
+    <!-- <template v-if="total <= 5">
+      <div :class="i === page ? 'sim-btn-num sim-btn-active' : 'sim-btn-num'" v-for="i in total">
         <span>{{ i }}</span>
       </div>
-    </template>
-    <template v-else>
-      <div :class="i=== page ? 'sim-btn-num sim-btn-active':'sim-btn-num'" v-for="i in 3">
-        <span>{{ i }}</span>
+    </template> -->
+    <template>
+      <div :class="1 === page ? 'sim-btn-num sim-btn-active' : 'sim-btn-num'" @click="gotoPage(1)">
+        <span>1</span>
       </div>
-      <div class="sim-btn-omit">
+      <div class="sim-btn-omit" @click="omitLeft" v-show="continuousNumList[0] > 2">
         <span>...</span>
       </div>
-      <div class="sim-btn-num">{{ total }}</div>
+      <div :class="i === page ? 'sim-btn-num sim-btn-active' : 'sim-btn-num'" v-for="i in continuousNumList" @click="gotoPage(i)">
+        <span>{{ i }}</span>
+      </div>
+      <div class="sim-btn-omit" @click="omitRight" v-show="continuousNumList[continuousNumList.length - 1] < total - 1">
+        <span>...</span>
+      </div>
+      <div :class="total === page ? 'sim-btn-num sim-btn-active' : 'sim-btn-num'" @click="gotoPage(total)">
+        <span>{{ total }}</span>
+      </div>
     </template>
     <div class="sim-btn-icon" @click="next">
       <SvgIcon className="svgIcon" iconName="right" />
@@ -36,6 +44,7 @@
 import cls from 'classnames'
 
 export default {
+  name: 'Page',
   props: {
     type: String,
     rootClass: String,
@@ -43,43 +52,11 @@ export default {
     page: Number,
     recordsCount: Number
   },
-  name: 'Page',
   data() {
     return {
       rootClasses: '',
-      toPage: ''
-    }
-  },
-  created() {
-    this.rootClasses = cls('page-wrapper', this.rootClass)
-  },
-  methods: {
-    first() {
-      // console.log('ffff')
-      this.$emit('goto', 1)
-    },
-    last() {
-      this.$emit('goto', this.total)
-    },
-    previous() {
-      if (this.page <= 1) {
-        return
-      }
-      this.$emit('goto', this.page - 1)
-    },
-    next() {
-      if (this.page >= this.total) {
-        return
-      }
-      this.$emit('goto', this.page + 1)
-    },
-    to(e) {
-      if (String(this.toPage).match(/^\d+$/)) {
-        const pageNumber = parseInt(this.toPage)
-        if (pageNumber > 0 && pageNumber <= this.total && pageNumber !== this.page) {
-          this.$emit('goto', parseInt(this.toPage))
-        }
-      }
+      toPage: '',
+      continuousNumList: [2, 3, 4, 5]
     }
   },
   computed: {
@@ -118,6 +95,98 @@ export default {
       }
       return pageArray
     }
+  },
+  watch: {
+    page: {
+      handler(val) {
+        if (!this.continuousNumList.includes(val)) this.continuousNum()
+      },
+      immediate: true
+    }
+  },
+  created() {
+    this.rootClasses = cls('page-wrapper', this.rootClass)
+  },
+  methods: {
+    first() {
+      // console.log('ffff')
+      this.$emit('goto', 1)
+    },
+    last() {
+      this.$emit('goto', this.total)
+    },
+    previous() {
+      if (this.page <= 1) {
+        return
+      }
+      this.$emit('goto', this.page - 1)
+    },
+    next() {
+      if (this.page >= this.total) {
+        return
+      }
+      this.$emit('goto', this.page + 1)
+    },
+    to(e) {
+      if (String(this.toPage).match(/^\d+$/)) {
+        const pageNumber = parseInt(this.toPage)
+        if (pageNumber > 0 && pageNumber <= this.total && pageNumber !== this.page) {
+          this.$emit('goto', parseInt(this.toPage))
+        }
+      }
+    },
+    continuousNum() {
+      switch (true) {
+        case [1, 2, 3, 4, 5].includes(this.page):
+          this.continuousNumList = [2, 3, 4, 5]
+          break
+        case [this.total - 4, this.total - 3, this.total - 2, this.total - 1, this.total].includes(this.page):
+          this.continuousNumList = [this.total - 4, this.total - 3, this.total - 2, this.total - 1]
+          break
+        default:
+          let arr = []
+          let Q = Math.floor((this.page - this.continuousNumList[0]) / 4)
+          // let R = (this.page - this.continuousNumList[0]) % 4
+          for (var i = 0; i < this.continuousNumList.length; i++) {
+            arr.push(this.continuousNumList[i] + 4 * Q)
+          }
+          this.continuousNumList = arr
+          break
+      }
+    },
+    gotoPage(i) {
+      this.$emit('goto', i)
+    },
+    omitLeft() {
+      switch (true) {
+        case this.page === this.total:
+          this.$emit('goto', this.total - 5)
+          break
+        default:
+          if (this.page - 4 > 1) {
+            this.$emit('goto', this.page - 4)
+          } else {
+            this.$emit('goto', 1)
+          }
+          break
+      }
+      this.continuousNum()
+    },
+    omitRight() {
+      switch (true) {
+        case this.page === 1:
+          this.$emit('goto', 6)
+          break
+        default:
+          if (this.page + 4 < this.total) {
+            this.$emit('goto', this.page + 4)
+          } else {
+            this.$emit('goto', this.total)
+          }
+          break
+      }
+      this.continuousNum()
+    }
   }
 }
 </script>
@@ -150,7 +219,7 @@ export default {
 }
 .sim-btn-active {
   color: #ffffff;
-  background-color: #2F80ED;
+  background-color: #2f80ed;
 }
 
 .page-wrapper {
