3ee70f59a80c73c098cd0847f14de0b5ced15aa5
‘baichenyang’ <baichenyang_ah@bonc.com.cn>
2023-08-23 10:28:08 +0800

    Head&Description

--- pages/transactions/_id.vue
+++ pages/transactions/_id.vue
@@ -1,204 +1,193 @@
 <template>
   <div class="root">
-    <nav-bar :active="5" />
-    <div class="page-container container">
-      <div class="title">
-        <h1>TRANSACTION DETAILS</h1>
-      </div>
-      <panel>
-        <template v-slot:header>
-          <span>Overview</span>
+    <Head title="TRANSACTION DETAILS"></Head>
+    <panel>
+      <v-table class="v-table" :headers="listSchema" :data="data">
+        <template v-slot:fee="{ data }">
+          <span v-if="data">{{ data | unit(isTest) }}</span>
+          <span v-else>{{ 0 | unit(isTest) }}</span>
         </template>
-        <v-table class="v-table" :headers="listSchema" :data="data">
-          <template v-slot:fee="{data}">
-            <span v-if="data">{{data | unit(isTest)}}</span>
-            <span v-else>{{0 | unit(isTest)}}</span>
-          </template>
 
-          <template v-slot:status="{data}">
-            <span v-if="data.status" class="status-success">Success</span>
-            <div v-else>
-              <span class="status-fail">Fail</span>
-              <span class="error-message">{{ data.error }}</span>
-            </div>
-          </template>
-          <template v-slot:timestamp="{data}">
-            <span>{{data * 1000 | timeFormat}} ( {{data * 1000 | timeFormat2}} )</span>
-          </template>
-        </v-table>
-      </panel>
-      <panel class="trx-panel">
-        <template v-slot:header>
-          <span>Contents</span>
+        <template v-slot:status="{ data }">
+          <span v-if="data.status" class="status-success">Success</span>
+          <div v-else>
+            <span class="status-fail">Fail</span>
+            <span class="error-message">{{ data.error }}</span>
+          </div>
         </template>
-        <v-table  v-if="data.method === 'staking.Transfer'" class="v-table" :headers="fromToSchema" :data="data">
-          <template v-slot:amount="{data}">
-            <span v-if="data">{{data | unit(isTest)}}</span>
-            <span v-else>{{0 | unit(isTest)}}</span>
-          </template>
-        </v-table>
-        <div class="raw-data" v-else>
-          <pre> {{data.raw | pretty}}</pre>
-        </div>
-      </panel>
-    </div>
+        <template v-slot:timestamp="{ data }">
+          <span>{{ (data * 1000) | timeFormat }} ( {{ (data * 1000) | timeFormat2 }} )</span>
+        </template>
+      </v-table>
+    </panel>
+    <panel title="Contents" class="trx-panel">
+      <v-table v-if="data.method === 'staking.Transfer'" class="v-table" :headers="fromToSchema" :data="data">
+        <template v-slot:amount="{ data }">
+          <span v-if="data">{{ data | unit(isTest) }}</span>
+          <span v-else>{{ 0 | unit(isTest) }}</span>
+        </template>
+      </v-table>
+      <div class="raw-data" v-else>
+        <pre> {{ data.raw | pretty }}</pre>
+      </div>
+    </panel>
   </div>
 </template>
 
 <script>
-  import Panel from '../../components/panel/Panel'
-  import VTable from '../../components/VTable/index'
-  import NavBar from '../../components/NavigationBar'
-  import { fetchTransactionDetail } from '~/fetch/index'
-  export default {
-    name: 'transactionDetail',
-    components: { NavBar, Panel, VTable},
-    async asyncData({ $axios, store: $store, params }) {
-      console.log('params', params)
-      const data = await fetchTransactionDetail({ $axios, $store }, params.id)
-      return {
-        data
-      }
-    },
-    data() {
-      return {
-        listSchema: [
-          {
-            label: 'Tx Hash',
-            key: 'txHash'
-          },
-          {
-            label: 'Status',
-            key: 'status',
-            slot: true
-          },
-          {
-            label: 'Time',
-            key: 'timestamp',
-            slot: true
-          },
-          {
-            label: 'Height',
-            key: 'height'
-          },
-          {
-            label: 'Fee',
-            key: 'fee',
-            slot: true
-          },
-          {
-            label: 'Nonce',
-            key: 'nonce'
-          },
-          {
-            label: 'Type',
-            key: 'method'
-          },
-        ],
-        list: [],
-        fromToSchema: [
-          {
-            label: 'From',
-            key: 'from'
-          },
-          {
-            label: 'To',
-            key: 'to'
-          },
-          {
-            label: 'Amount',
-            key: 'amount',
-            slot: true
-          }
-        ]
-      }
-    },
-    async mounted() {
-    },
-    methods: {
+import Head from '~/components/Head'
+import Panel from '../../components/panel/Panel'
+import Description from '~/components/description/index.vue'
+import VTable from '../../components/VTable/index'
+import { fetchTransactionDetail } from '~/fetch/index'
+export default {
+  name: 'transactionDetail',
+  components: { Head, Panel, Description, VTable },
+  async asyncData({ $axios, store: $store, params }) {
+    console.log('params', params)
+    const data = await fetchTransactionDetail({ $axios, $store }, params.id)
+    return {
+      data
     }
-  }
+  },
+  data() {
+    return {
+      listSchema: [
+        {
+          label: 'Tx Hash',
+          key: 'txHash'
+        },
+        {
+          label: 'Status',
+          key: 'status',
+          slot: true
+        },
+        {
+          label: 'Time',
+          key: 'timestamp',
+          slot: true
+        },
+        {
+          label: 'Height',
+          key: 'height'
+        },
+        {
+          label: 'Fee',
+          key: 'fee',
+          slot: true
+        },
+        {
+          label: 'Nonce',
+          key: 'nonce'
+        },
+        {
+          label: 'Type',
+          key: 'method'
+        }
+      ],
+      list: [],
+      fromToSchema: [
+        {
+          label: 'From',
+          key: 'from'
+        },
+        {
+          label: 'To',
+          key: 'to'
+        },
+        {
+          label: 'Amount',
+          key: 'amount',
+          slot: true
+        }
+      ]
+    }
+  },
+  computed: {
+    descriptionList() {
+      return []
+    }
+  },
+  async mounted() {},
+  methods: {}
+}
 </script>
 
 <style scoped lang="scss">
-  .root {
-    background-color: #f7f7f7;
-    padding-bottom: rem(50);
-    min-height: calc(100vh - #{rem(100)});
+.raw-data {
+  background: #f8f9fa;
+  border: 1px solid #b2b2b2;
+  border-radius: rem(4);
+  max-height: rem(400);
+  overflow-y: scroll;
+  padding: rem(15) rem(20);
+  margin-top: rem(30);
+  margin-left: rem(20);
+  margin-right: rem(20);
+  pre {
+    white-space: pre-wrap;
+    word-wrap: break-word;
   }
-  .raw-data {
-    background: #F8F9FA;
-    border: 1px solid #B2B2B2;
-    border-radius: rem(4);
-    max-height: rem(400);
-    overflow-y: scroll;
-    padding: rem(15) rem(20);
-    margin-top: rem(30);
-    margin-left: rem(20);
-    margin-right: rem(20);
-    pre {
-      white-space: pre-wrap;
-      word-wrap: break-word;
+}
+.v-table {
+  margin-top: rem(16);
+}
+.container {
+}
+.title {
+  padding-top: rem(20);
+  padding-bottom: rem(20);
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  h1 {
+    font-size: rem(20);
+    padding: 0;
+    margin: 0;
+    @include regular;
+    color: black;
+    font-weight: normal;
+    span {
+      font-size: rem(14);
+      color: rgba(0, 0, 0, 0.5);
     }
   }
-  .v-table {
-    margin-top: rem(16);
+}
+.trx-panel {
+  margin-top: rem(20);
+  .block-total-list {
+    width: 100%;
+    margin-left: 0;
   }
-  .container {
+}
+.no-result {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  font-size: 14px;
+  color: rgba(55, 65, 107, 1);
+  padding: rem(80) 0;
+  .empty-icon {
+    width: rem(80);
+    margin-bottom: rem(11);
   }
-  .title {
-    padding-top: rem(20);
-    padding-bottom: rem(20);
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    h1 {
-      font-size: rem(20);
-      padding: 0;
-      margin: 0;
-      @include regular;;
-      color: black;
-      font-weight: normal;
-      span {
-        font-size: rem(14);
-        color: rgba(0, 0, 0, 0.5);
-      }
-    }
-  }
-  .trx-panel {
-    margin-top: rem(12);
-    .block-total-list {
-      width: 100%;
-      margin-left: 0;
-    }
-  }
-  .no-result {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    text-align: center;
-    font-size: 14px;
-    color: rgba(55, 65, 107, 1);
-    padding: rem(80) 0;
-    .empty-icon {
-      width: rem(80);
-      margin-bottom: rem(11);
-    }
-  }
+}
 
-  .status-fail,.status-success {
-    padding: rem(4) rem(10);
-    color: white;
-    border-radius: rem(4);
-    font-size: rem(12);
-  }
-  .status-fail {
-    background-color: #F7685B;
-  }
-  .error-message {
-    color: #F7685B;
-  }
-  .status-success {
-    background-color: #2ED47A;
-  }
+.status-fail,
+.status-success {
+  padding: rem(4) rem(10);
+  color: white;
+  border-radius: rem(4);
+  font-size: rem(12);
+}
+.status-fail {
+  background-color: #f7685b;
+}
+.error-message {
+  color: #f7685b;
+}
+.status-success {
+  background-color: #2ed47a;
+}
 </style>
--- pages/transactions/index.vue
+++ pages/transactions/index.vue
@@ -1,256 +1,231 @@
 <template>
   <div class="blocks-root">
-    <nav-bar :active="5"/>
-    <div class="page-container container">
-      <div class="title">
-        <h1>TRANSACTIONS<span class="total-count"> ({{totalTxs | readable}})</span></h1>
+    <Head title="TRANSACTIONS">
+      <template #HeadLeft>
+        <span class="total-count"> ({{ totalTxs | readable }})</span>
+      </template>
+    </Head>
+    <Dropdown trigger="click" placement="bottom-start" @on-click="change">
+      <a class="show-cur method-dropdown" href="javascript:void(0)">
+        {{ method === '' ? 'All Type' : method }}
+        <Icon type="ios-arrow-down"></Icon>
+      </a>
+      <DropdownMenu slot="list">
+        <DropdownItem name="all">All Type</DropdownItem>
+        <DropdownItem v-for="method in methods" :key="method" :name="method">{{ method }}</DropdownItem>
+      </DropdownMenu>
+    </Dropdown>
+    <div class="block-list-wrapper">
+      <block-table root-class="block-total-list" cell-class="block-total-list-cell" :columns="columns" :data="list">
+        <template v-slot:fee="{ data }">
+          <span v-if="data">{{ data | unit(isTest) }}</span>
+          <span v-else>0</span>
+        </template>
+        <template v-slot:status="{ data }">
+          <span v-if="data" class="status-success">Success</span>
+          <span v-else class="status-fail" :data-data="data">Fail</span>
+        </template>
+        <template v-slot:timestamp="{ data }">
+          <span>{{ data.value | timeFormat }} </span>
+        </template>
+      </block-table>
+      <div class="page-navigation">
+        <page :sizer="sizer" :records-count="total" :page="page" root-class="block-page" @goto="goto"></page>
       </div>
-      <Dropdown trigger="click" placement="bottom-start" @on-click="change">
-        <a class="show-cur method-dropdown" href="javascript:void(0)">
-          {{method === '' ? 'All Type' : method}}
-          <Icon type="ios-arrow-down"></Icon>
-        </a>
-        <DropdownMenu slot="list">
-          <DropdownItem name="all">All Type</DropdownItem>
-          <DropdownItem v-for="method in methods" :key="method" :name="method">{{method}}</DropdownItem>
-        </DropdownMenu>
-      </Dropdown>
-      <div class="block-list-wrapper">
-        <block-table root-class="block-total-list" cell-class="block-total-list-cell" :columns="columns" :data="list">
-          <template v-slot:fee="{data}">
-            <span v-if="data">{{data | unit(isTest)}}</span>
-            <span v-else>0</span>
-          </template>
-          <template v-slot:status="{data}">
-            <span v-if="data" class="status-success">Success</span>
-            <span v-else class="status-fail" :data-data="data">Fail</span>
-          </template>
-          <template v-slot:timestamp="{data}">
-            <span>{{data.value | timeFormat}} </span>
-          </template>
-        </block-table>
-        <div class="page-navigation">
-          <page :sizer="sizer" :records-count="total" :page="page" root-class="block-page" @goto="goto"></page>
-        </div>
-      </div>
-      <!--<div>{{JSON.stringify(list)}}</div>-->
     </div>
   </div>
 </template>
 
 <script>
-  import {fetchTransactionsList, fetchChainMethods, fetchBlockInfo} from '../../fetch/index'
-  import BlockTable from '../../components/Table/index'
-  import NavBar from '../../components/NavigationBar'
-  import Page from '../../components/Page'
+import { fetchTransactionsList, fetchChainMethods, fetchBlockInfo } from '../../fetch/index'
+import BlockTable from '../../components/Table/index'
+import Head from '~/components/Head'
+import Page from '../../components/Page'
 
-  export default {
-    name: 'index',
-    components: {
-      NavBar,
-      BlockTable,
-      Page
+export default {
+  name: 'index',
+  components: {
+    Head,
+    BlockTable,
+    Page
+  },
+  async asyncData({ $axios, store: $store }) {
+    const res = await Promise.all([fetchBlockInfo({ $axios, $store }), fetchTransactionsList({ $axios, $store }, 1, 20, '', true, 12)])
+    const { totalTxs = 0 } = res[0]
+    const { list, totalSize } = res[1]
+    return { list, total: totalSize, totalTxs }
+  },
+  methods: {
+    async goto(pageNumber, progress = true) {
+      const { $axios, $store } = this
+      if (pageNumber > 1) {
+        this.timer && clearTimeout(this.timer)
+        this.timer = null
+      }
+      const { list, totalSize } = await fetchTransactionsList({ $axios, $store }, pageNumber, this.sizer, this.method, progress, 12)
+      this.page = pageNumber
+      this.list = list
+      this.total = totalSize
+      if (progress) {
+        document.documentElement.scrollTop = document.body.scrollTop = 0
+      }
+      if (this.page === 1) {
+        this.repull()
+      }
     },
-    async asyncData({ $axios, store: $store }) {
-      const res = await Promise.all([
-        fetchBlockInfo({ $axios, $store }),
-        fetchTransactionsList({ $axios, $store }, 1, 20, '', true, 12)
-      ])
-      const { totalTxs = 0 } = res[0];
-      const { list, totalSize } = res[1];
-      return { list, total: totalSize, totalTxs }
+    change(name) {
+      console.log('name', name)
+      if (name === 'all') {
+        this.method = ''
+      } else {
+        this.method = name
+      }
+      this.goto(1)
     },
-    methods: {
-      async goto(pageNumber, progress = true) {
-        const { $axios, $store } = this
-        if (pageNumber > 1) {
-          this.timer && clearTimeout(this.timer)
-          this.timer = null
-        }
-        const { list, totalSize } = await fetchTransactionsList({ $axios, $store }, pageNumber, this.sizer, this.method, progress, 12)
-        this.page = pageNumber
-        this.list = list
-        this.total = totalSize
-        if (progress) {
-          document.documentElement.scrollTop = document.body.scrollTop = 0
-        }
+    repull() {
+      this.timer && clearTimeout(this.timer)
+      this.timer = setTimeout(async () => {
         if (this.page === 1) {
+          await this.goto(1, false)
           this.repull()
         }
-      },
-      change(name) {
-        console.log('name', name)
-        if (name === 'all') {
-          this.method = ''
-        } else {
-          this.method = name
+      }, 6000)
+    }
+  },
+  computed: {},
+  created() {},
+  async mounted() {
+    const { $axios, $store } = this
+    const { list } = await fetchChainMethods({ $axios, $store })
+    this.methods = list
+    this.repull()
+  },
+  data() {
+    return {
+      sizer: 20,
+      page: 1,
+      list: [],
+      name: '',
+      methods: [],
+      method: '',
+      columns: [
+        {
+          title: 'Tx Hash',
+          key: 'txHash'
+        },
+        {
+          title: 'Height',
+          key: 'height'
+        },
+        {
+          title: 'Type',
+          key: 'method'
+        },
+        {
+          title: 'Fee',
+          key: 'fee',
+          slot: true
+        },
+        {
+          title: 'Status',
+          key: 'status',
+          slot: true
+        },
+        {
+          title: 'Time',
+          key: 'timestamp',
+          slot: true
         }
-        this.goto(1)
-      },
-      repull() {
-        this.timer && clearTimeout(this.timer)
-        this.timer = setTimeout(async () => {
-          if (this.page === 1) {
-            await this.goto(1, false)
-            this.repull()
-          }
-        }, 6000)
-      }
-    },
-    computed: {
-    },
-    created() {
-    },
-    async mounted() {
-      const { $axios, $store } = this
-      const { list } = await fetchChainMethods({ $axios, $store })
-      this.methods = list
-      this.repull()
-    },
-    data() {
-      return {
-        sizer: 20,
-        page: 1,
-        list: [],
-        name: '',
-        methods: [],
-        method: '',
-        columns: [
-          {
-            title: 'Tx Hash',
-            key: 'txHash'
-          },
-          {
-            title: 'Height',
-            key: 'height'
-          },
-          {
-            title: 'Type',
-            key: 'method'
-          },
-          {
-            title: 'Fee',
-            key: 'fee',
-            slot: true
-          },
-          {
-            title: 'Status',
-            key: 'status',
-            slot: true
-          },
-          {
-            title: 'Time',
-            key: 'timestamp',
-            slot: true
-          }
-        ]
-      }
+      ]
     }
   }
+}
 </script>
 
 <style scoped lang="scss">
-  .method-dropdown {
-    margin-top: rem(20);
-    padding: rem(2) rem(10);
-    border: 1px solid rgba(0, 0, 0, 0.5);
-    display: inline-block;
-    border-radius: rem(4);
-    background-color: #ffffff;
-    .show-cur {
-      color: #5F5F5F;
+.method-dropdown {
+  margin-top: rem(20);
+  padding: rem(2) rem(10);
+  border: 1px solid rgba(0, 0, 0, 0.5);
+  display: inline-block;
+  border-radius: rem(4);
+  background-color: #ffffff;
+  .show-cur {
+    color: #5f5f5f;
+  }
+}
+.status-fail,
+.status-success {
+  padding: rem(4) rem(10);
+  color: white;
+  border-radius: rem(4);
+  font-size: rem(12);
+}
+.status-fail {
+  background-color: #f7685b;
+}
+.status-success {
+  background-color: #2ed47a;
+}
+.blocks-root {
+  background-color: #f7f7f7;
+  min-height: calc(100vh - #{rem(100)});
+}
+.block-list-wrapper {
+  margin-top: rem(12);
+  background-color: white;
+  padding: 0 rem(30);
+  border-radius: rem(8);
+  .block-total-list {
+    padding: 0;
+    width: 100%;
+    margin-left: 0;
+    border-radius: 1px;
+    /deep/ td,
+    /deep/ th {
+      vertical-align: middle;
+      padding: 18px 10px;
     }
-  }
-  .status-fail,.status-success {
-    padding: rem(4) rem(10);
-    color: white;
-    border-radius: rem(4);
-    font-size: rem(12);
-  }
-  .status-fail {
-    background-color: #F7685B;
-  }
-  .status-success {
-    background-color: #2ED47A;
-  }
-  .blocks-root {
-    background-color: #f7f7f7;
-    min-height: calc(100vh - #{rem(100)});
-  }
-  .container {
-    padding-bottom: rem(50);
+    /deep/ tr th,
+    /deep/ tr td {
+      &:nth-child(1) {
+        width: rem(240);
+      }
+      &:last-child {
+        padding-left: 0;
+        width: 100px;
+      }
+    }
   }
   .title {
-    padding-top: rem(20);
+    margin-left: 0px;
+    margin-top: 6px;
+    font-size: 18px;
+    font-family: PingFangSC-Regular;
+    font-weight: 400;
+    color: rgba(55, 65, 107, 1);
+    line-height: 1;
     display: flex;
     align-items: center;
-    justify-content: space-between;
-    h1 {
-      font-size: rem(20);
-      padding: 0;
-      margin: 0;
-      @include regular;;
-      color: black;
-      font-weight: normal;
-      span {
-        font-size: rem(14);
-        color: rgba(0, 0, 0, 0.5);
-      }
+    > .icon {
+      width: 30px;
+      height: 30px;
+      margin-right: 10px;
     }
   }
-  .block-list-wrapper {
-    margin-top: rem(12);
-    background-color: white;
-    padding:0 rem(30);
-    border-radius: rem(8);
-    .block-total-list{
-      padding: 0;
-      width: 100%;
-      margin-left: 0;
-      border-radius: 1px;
-      /deep/ td, /deep/ th {
-        vertical-align: middle;
-        padding: 18px 10px;
-      }
-      /deep/ tr th, /deep/ tr td{
-        &:nth-child(1) {
-          width: rem(240)
-        }
-        &:last-child {
-          padding-left: 0;
-          width: 100px;
-        }
-      }
-    }
-    .title {
-      margin-left: 0px;
-      margin-top: 6px;
-      font-size:18px;
-      font-family:PingFangSC-Regular;
-      font-weight:400;
-      color:rgba(55,65,107,1);
-      line-height: 1;
-      display: flex;
-      align-items: center;
-      > .icon {
-        width: 30px;
-        height: 30px;
-        margin-right: 10px;
-      }
-    }
-    .total-records{
-      margin-left: 40px;
-      margin-top: 9px;
-      font-size:12px;
-      font-family:PingFangSC-Regular;
-      font-weight:400;
-      color:rgba(55,65,107,0.5);
-      line-height: 1;
-    }
+  .total-records {
+    margin-left: 40px;
+    margin-top: 9px;
+    font-size: 12px;
+    font-family: PingFangSC-Regular;
+    font-weight: 400;
+    color: rgba(55, 65, 107, 0.5);
+    line-height: 1;
   }
-  .page-navigation {
-    display: flex;
-    justify-content: center;
-    padding: 15px 0;
-  }
+}
+.page-navigation {
+  display: flex;
+  justify-content: center;
+  padding: 15px 0;
+}
 </style>
